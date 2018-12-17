c3053ac5ff794748a8a50ee534681eef38bef831
Koalakai <kaishinpk@gmail.com>
2018-12-16 17:16:42 -0800

    Do Express/React tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    data:null,
+  };
+
+  componentDidMount(){
+    this.callBackEndAPI()
+      .then(res=> this.setState({data:res.express}))
+      .catch(err=> console.log(err));
+  }
+
+  callBackEndAPI = async() => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if( response.status !== 200 ){
+      throw Error(body.message)
+    }
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
