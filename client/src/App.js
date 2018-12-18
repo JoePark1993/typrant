84eb74b5f6d08cd84587503621940e9b857ee89a
Koalakai <kaishinpk@gmail.com>
2018-12-17 19:55:57 -0800

    Add some routing. Introduced phaser bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-import PhaserContainer from './PhaserContainer.js';
+import{
+  Link,
+  BrowserRouter,
+  Route,
+} from 'react-router-dom'
+
+import Play from './Play/Play.js';
+import Home from './Home.js';
+import Login from './Login/Login.js';
 
 class App extends Component {
-  state={
-    data:null,
-  };
 
   componentDidMount(){
     this.callBackEndAPI()
@@ -26,7 +30,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <PhaserContainer/>
+        <BrowserRouter>
+          <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/Play/Play'>Play</Link></li>
+            <li><Link to='/Login/Login'>Login</Link></li>
+          </ul>
+          <Route path="/" exact={true} component={Home}/>
+          <Route 
+            path="/Play/Play"
+            render={(props)=><Play/>}
+          />
+          <Route 
+            path="/Login/Login"
+            render={(props)=><Login/>}
+          />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -9,3 +9,114 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+var players = {};
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+
+  console.log('a user connected');
+
+
+  //HELP PLS TEAM
+  players[socket.id]={
+    x: Math.floor(Math.random() * 420 ) + 40,
+    y: Math.floor(Math.random() * 420 ) + 40,
+    /*
+    x: Math.floor(Math.random() * 100 ) + 40,
+    y: Math.floor(Math.random() * 100 ) + 40,
+    */
+    playerId: socket.id,
+    mass: 50,
+    inBattle: false,
+    username: generateName(),
+
+    //^^^you might want to make this the socket.id in the mean time
+    };
+
+  //send list of players to new player
+  socket.emit('currentPlayers',players);
+  //update current players with new player
+  socket.broadcast.emit( 'newPlayer' , players[socket.id]);
+
+
+  //handle if a user leaves the game
+  socket.on('disconnect', function(){
+    console.log('user disconnect');
+    delete players[socket.id];
+    io.emit('disconnect',socket.id);
+
+  });
+
+  socket.on('playerMovement', function( movementData){
+    players[socket.id].x=movementData.x;
+    players[socket.id].y=movementData.y;
+    socket.broadcast.emit('playerMoved',players[socket.id]);
+  });
+
+  socket.on('p2pHit', function( playerId , otherPlayerId ){
+    players[playerId].inBattle = true;
+    players[otherPlayerId].inBattle = true;
+    socket.broadcast.emit('playerBattle',players[socket.id]);
+    //change to io.emit
+    socket.broadcast.emit('playerBattle',players[otherPlayerId]);
+    socket.broadcast.to(otherPlayerId).emit('p2pBattle', playerId );
+    socket.emit( 'p2pBattle', otherPlayerId);
+  });
+
+  socket.on('typeSceneEnd', function( playerId , otherPlayerId ){
+    socket.broadcast.to(otherPlayerId).emit('lose' );
+    players[playerId].inBattle = false;
+    players[otherPlayerId].inBattle = false;
+    socket.broadcast.emit('playerBattle',players[socket.id]);
+    io.emit('playerBattle',players[otherPlayerId]);
+
+    //include mass after
+    socket.broadcast.to(otherPlayerId).emit('battleOutCome' );
+    socket.emit( 'battleOutCome' );
+
+    let loserMass = players[otherPlayerId].mass;
+    let winnerMass = players[playerId].mass;
+    if( loserMass > winnerMass ){
+      loserMass -= winnerMass;
+      winnerMass += winnerMass;
+    }
+    else{
+      winnerMass += loserMass ;
+      loserMass = 1;
+    }
+
+    players[playerId].mass= winnerMass;
+    players[otherPlayerId].mass= loserMass;
+
+    io.emit('massUpdate', players[playerId] );
+    io.emit('massUpdate', players[otherPlayerId] );
+  });
+
+
+});
+
+
+
+/*
+server.listen(8081, function () {
+    console.log(`Listening on ${server.address().port}`);
+});
+*/
+
+function generateName(){
+var randomNames = ["Harry", "Joe", "Bob", "David", "Frodo", "Luke", "Tarzan", "Michalis", "Mufasa" ];
+var rand = randomNames[Math.floor(Math.random() * randomNames.length)];
+return rand+Math.floor(Math.random() * 100) + 1;
+};
+
+//server.listen( process.env.PORT,process.env.IP);
