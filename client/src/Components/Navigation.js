015a7bfb03a0a49789d801a6223ca553ef6146e0
Lucas Song <lucas7song@gmail.com>
2019-01-08 13:33:39 -0800

    updating to typrant navbar

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -4,7 +4,7 @@ class Navigation extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Navbar</a>
+        <a className="navbar-brand" href="/">Typrant</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -15,21 +15,24 @@ class Navigation extends React.Component {
               <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">Link</a>
+              <a className="nav-link" href="/Play/Play">Play</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Login/Login">Login</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Register</a>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
+                User
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/">Action</a>
+                <a className="dropdown-item" href="/">Settings</a>
                 <a className="dropdown-item" href="/">Another action</a>
                 <div className="dropdown-divider"></div>
                 <a className="dropdown-item" href="/">Something else here</a>
               </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="/">Disabled</a>
             </li>
           </ul>
           <form className="form-inline my-2 my-lg-0">
