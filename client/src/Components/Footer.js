fcfac43cefe103656e508bfdf678af8bc6a69755
Lucas Song <lucas7song@gmail.com>
2019-01-11 08:38:41 -0800

    basic footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -4,9 +4,15 @@ class StickyFooter extends React.Component {
     render() {
         return (
             <footer class = "page-footer font-small indigo">
-            <div class="footer-copyright text-center py-3">© 2018 Copyright:
-    <a href="typrant.comsssssssssssssssss"> Typrant</a>
-  </div>
+            <div class="footer-copyright text-center py-3">
+            <a href="typrant.com"> Typrant </a>
+            <a href='./about'> About </a>
+            <a href='./contribute'> Contribute </a>
+            <a href='./contact'> Contact </a>
+            <a href='./login'> Login </a>
+            <a href='./buy'> Buy </a>
+
+            </div>
             </footer>
         )
     }
